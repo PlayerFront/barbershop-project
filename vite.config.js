af5bb3b256c2d4674dfb6483cb83eb1ff6ffd5be
Elena <elena.wictorovna@gmail.com>
2025-11-06 18:49:07 +0300

    optimized site

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 
 export default defineConfig({
   plugins: [
     react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: injectData
+      }
+    })
   ],
   server: {
     host: true
@@ -12,5 +19,17 @@ export default defineConfig({
   base: './',
   build: {
     cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          swiper: ['swiper', 'swiper/react']
+        }
+      }
+    },
+    sourcemap: false
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
   }
 })
